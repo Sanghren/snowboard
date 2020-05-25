30f565eb6f87e6e25eef561948cde0ca7a79c579
tbrunain <tristan@settlemint.com>
2020-05-25 15:49:46 +0200

    feat: Improve the metrics view

--- src/views/NodeMetrics.vue
+++ src/views/NodeMetrics.vue
@@ -1,29 +1,31 @@
 <template>
-    <v-container class="#121212 lighten-5">
-        <v-row no-gutters
-               v-for="(metric, index) in this.$store.state.Metrics.metrics"
-               :key="index"
-        >
-            <v-col :key="index">
-                <v-card
-                        class="pa-2"
-                        outlined
-                        tile
-                >
-                    <div v-if="metric.type === 'HISTOGRAM'">
-                        <p><b>{{metric.help}}</b></p>
-                        <histogram-chart :chartdata="{labels: Object.keys(metric.metrics[0].buckets),
+    <v-container class="theme--dark lighten-5">
+        <template v-for="(chunk, rowIndex) in productChunks">
+            <v-row :key="rowIndex">
+                <v-col
+                        v-for="(metric, index) in chunk"
+                        :key="index"
+                       cols="4"
+                       md="4">
+                    <v-card
+                            class="pa-2"
+                            outlined
+                            tile
+                    >
+                        <div v-if="metric.type === 'HISTOGRAM'">
+                            <p><b>{{metric.help}}</b></p>
+                            <histogram-chart :chartdata="{labels: Object.keys(metric.metrics[0].buckets),
                 datasets: [
       {
         label: metric.name,
         data: Object.values(metric.metrics[0].buckets)
       }
     ]}">
-                        </histogram-chart>
-                    </div>
-                    <div v-if="metric.type === 'GAUGE'">
-                        <p><b>{{metric.help}}</b></p>
-                        <doughnut-chart :chartdata="{
+                            </histogram-chart>
+                        </div>
+                        <div v-if="metric.type === 'GAUGE'">
+                            <p><b>{{metric.help}}</b></p>
+                            <doughnut-chart :chartdata="{
                 labels: [ metric.name, 'unused'],
                 datasets: [
       {
@@ -32,37 +34,43 @@
         data: [metric.metrics[0].value, 100 - metric.metrics[0].value]
       }
     ]}"></doughnut-chart>
-                    </div>
-                    <div v-if="metric.type === 'COUNTER'">
-                        <p><b>{{metric.help}}</b></p>
-                        <p>Name : {{ metric.name }}</p>
-                        <p>Value : {{ Object.values(metric.metrics[0])[0] }}</p>
-                    </div>
-                    <div v-else>
-                    </div>
-                </v-card>
-            </v-col>
-            <v-responsive
-                    v-if="index === 2"
-                    :key="`width-${index}`"
-                    width="100%"
-            ></v-responsive>
-        </v-row>
+                        </div>
+                        <div v-if="metric.type === 'COUNTER'">
+                            <p><b>{{metric.help}}</b></p>
+                            <p>Name : {{ metric.name }}</p>
+                            <p>Value : {{ Object.values(metric.metrics[0])[0] }}</p>
+                        </div>
+                        <div v-else>
+                        </div>
+                    </v-card>
+                </v-col>
+                <v-responsive
+                        v-if="index === 2"
+                        :key="`width-${index}`"
+                        width="100%"
+                ></v-responsive>
+            </v-row>
+        </template>
     </v-container>
 </template>
 
 <script lang="ts">
-    import {Component, Prop, Vue} from 'vue-property-decorator';
+    import {Vue} from 'vue-property-decorator';
     import HistogramChart from "@/components/HistogramChart.vue";
     import DoughnutChart from "@/components/DoughnutChart.vue";
-    @Component({
-        components: {DoughnutChart, HistogramChart}
-    })
-    export default class Dashboard extends Vue {
-        @Prop() private msg!: string;
-        @Prop() private metrics!: any[];
+    import _ from 'lodash'
 
-    }
+    export default Vue.extend({
+        components: {DoughnutChart, HistogramChart},
+        props: ['msg', 'metrics'],
+        computed: {
+            productChunks(){
+                return _.chunk(Object.values(this.$store.state.Metrics.metrics), 3);
+            }
+        },
+        methods: {}
+    })
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
