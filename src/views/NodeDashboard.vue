6f4d187e9f75a7389c47c9e2865c76cd18053b7d
tbrunain <tristan@settlemint.com>
2020-05-25 20:30:38 +0200

    feat: Some improvement in Dashboard.vue

--- src/views/NodeDashboard.vue
+++ src/views/NodeDashboard.vue
@@ -1,22 +1,12 @@
 <template>
     <v-container class="theme--dark lighten-5">
-        <!-- Stack the columns on mobile by making one full-width and the other half-width -->
-        <v-row>
-            <v-col
-                    cols="18"
-                    md="12"
-                    class="text-center"
-            >
-                <h2>Dashboard</h2>
-            </v-col>
-        </v-row>
         <v-row>
             <v-col
                     cols="4"
                     md="3"
             >
                 <v-card
-                        class="pa-2"
+                        class="mx-auto"
                         height="100%"
                         outlined
                         tile
@@ -35,17 +25,17 @@
                         </v-tooltip>
                     </v-list-item>
                     <v-divider></v-divider>
-                    <v-card-text>
-                        {{ this.$store.state.Admin.nodeId }}
+                    <v-card-text class="text-center">
+                        <b>{{ this.$store.state.Admin.nodeId }}</b>
                     </v-card-text>
                 </v-card>
             </v-col>
             <v-col
-                    cols="4"
+                    cols="1"
                     md="3"
             >
                 <v-card
-                        class="pa-2"
+                        class="mx-auto"
                         height="100%"
                         outlined
                         tile
@@ -64,8 +54,8 @@
                         </v-tooltip>
                     </v-list-item>
                     <v-divider></v-divider>
-                    <v-card-text>
-                        {{ this.$store.state.Admin.networkId }}
+                    <v-card-text  height="100%" class="text-center" justify="center">
+                        <b class="text--primary">{{ this.$store.state.Admin.networkId }}</b>
                     </v-card-text>
                 </v-card>
             </v-col>
@@ -74,16 +64,14 @@
                     md="3"
             >
                 <v-card
-                        class="pa-2"
+                        class="mx-auto b"
                         height="100%"
-                        v-model="this.$store.state.Health.healthy"
-                        :class="this.$store.state.Health.healthy ? 'node-status-green-background' : 'node-status-red-background'"
                         outlined
                         tile
                 >
                     <v-list-item>
                         <v-list-item-content>
-                            <v-list-item-title class="headline">Node Status</v-list-item-title>
+                            <v-list-item-title class="headline">Peers</v-list-item-title>
                         </v-list-item-content>
                         <v-tooltip>
                             <template v-slot:activator="{ on }">
@@ -91,39 +79,72 @@
                                     help_outline
                                 </span>
                             </template>
-                            <span>Whether your node is marked as 'healthy'</span>
+                            <span>The number of peers to which this node is talking tp.</span>
                         </v-tooltip>
                     </v-list-item>
+                    <v-divider></v-divider>
+                    <v-card-text  class="text-center" justify="center">
+                        <b class="text--primary">{{this.$store.state.Admin.peers.length}}</b>
+                    </v-card-text>
                 </v-card>
-
             </v-col>
             <v-col
-                    cols="6"
+                    cols="4"
                     md="3"
             >
-                <v-card
-                        class="pa-2"
-                        height="100%"
-                        v-model="this.$store.state.XChain.balance.balance"
-                        :class="this.$store.state.XChain.balance.balance !== 0 ? 'node-status-green-background' : 'node-status-red-background'"
-                        outlined
-                        tile
-                >
-                    <v-list-item>
-                        <v-list-item-content>
-                            <v-list-item-title class="headline">Bootstrapped</v-list-item-title>
-                        </v-list-item-content>
-                        <v-tooltip>
-                            <template v-slot:activator="{ on }">
+                <v-container>
+                    <v-row>
+                        <v-card
+                                class="mb-2"
+                                height="100%"
+                                width="100%"
+                                v-model="this.$store.state.Health.healthy"
+                                :class="this.$store.state.Health.healthy ? 'node-status-green-background' : 'node-status-red-background'"
+                                outlined
+                                tile
+                        >
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title class="headline">Node Status</v-list-item-title>
+                                </v-list-item-content>
+                                <v-tooltip>
+                                    <template v-slot:activator="{ on }">
                                 <span v-on="on" class="material-icons">
                                     help_outline
                                 </span>
-                            </template>
-                            <span>We check the balance of the faucet to see whether the node is bootstrapped or not.
+                                    </template>
+                                    <span>Whether your node is marked as 'healthy'</span>
+                                </v-tooltip>
+                            </v-list-item>
+                        </v-card>
+                    </v-row>
+                    <v-row>
+                        <v-card
+                                class="mx-auto"
+                                height="100%"
+                                width="100%"
+                                v-model="this.$store.state.XChain.balance.balance"
+                                :class="this.$store.state.XChain.balance.balance !== 0 ? 'node-status-green-background' : 'node-status-red-background'"
+                                outlined
+                                tile
+                        >
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title class="headline">Bootstrapped</v-list-item-title>
+                                </v-list-item-content>
+                                <v-tooltip>
+                                    <template v-slot:activator="{ on }">
+                                <span v-on="on" class="material-icons">
+                                    help_outline
+                                </span>
+                                    </template>
+                                    <span>We check the balance of the faucet to see whether the node is bootstrapped or not.
                                 <a>https://docs.ava.network/v1.0/en/quickstart/ava-getting-started/</a></span>
-                        </v-tooltip>
-                    </v-list-item>
-                </v-card>
+                                </v-tooltip>
+                            </v-list-item>
+                        </v-card>
+                    </v-row>
+                </v-container>
             </v-col>
         </v-row>
 
@@ -150,6 +171,8 @@
         props: {},
         mounted() {
             this.$store.dispatch('Admin/fetchNodeId');
+            this.$store.dispatch('Admin/fetchNetworkIds');
+            this.$store.dispatch('Admin/fetchPeers');
             this.$store.dispatch('Metrics/fetchMetrics');
         },
         methods: {}
