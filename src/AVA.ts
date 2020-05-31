59498260dedc7894dbdc7d00a766c22f72fdff79
tbrunain <tristan@settlemint.com>
2020-05-31 18:10:39 +0200

    fix: Inverted chainId and networkId

--- src/AVA.ts
+++ src/AVA.ts
@@ -3,13 +3,13 @@ import {Config} from "@/types";
 
 const bootstrapNodeHost = process.env.VUE_APP_SNWBRD_BOOTSTRAP_HOST || 'bootstrap.ava.network';
 const bootstrapProtocol = process.env.VUE_APP_SNWBRD_BOOTSTRAP_PROTOCOL || 'https';
-const bootstrapChainId = process.env.VUE_APP_SNWBRD_BOOTSTRAP_CHAIN_ID || '2';
+const bootstrapChainId = process.env.VUE_APP_SNWBRD_BOOTSTRAP_CHAIN_ID || 'X';
 const bootstrapNodePort = process.env.VUE_APP_SNWBRD_BOOTSTRAP_PORT || '2100';
 const bootstrapNetworkId = process.env.VUE_APP_SNWBRD_BOOTSTRAP_NETWORK_ID || '4ktRjsAKxgMr2aEzv9SWmrU7Xk5FniHUrVCX4P1TZSfTLZWFM';
 
 const nodeHost = process.env.VUE_APP_SNWBRD_NODE_HOST || 'localhost';
 const protocol = process.env.VUE_APP_SNWBRD_NODE_PROTOCOL || 'http';
-const chainId = process.env.VUE_APP_SNWBRD_NODE_CHAIN_ID || '2';
+const chainId = process.env.VUE_APP_SNWBRD_NODE_CHAIN_ID || 'X';
 const nodePort = process.env.VUE_APP_SNWBRD_NODE_PORT || '9650';
 const networkId = process.env.VUE_APP_SNWBRD_NODE_NETWORK_ID || '4ktRjsAKxgMr2aEzv9SWmrU7Xk5FniHUrVCX4P1TZSfTLZWFM';
 
