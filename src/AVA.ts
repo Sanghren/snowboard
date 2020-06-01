85f6235b48e6a2e04651d127283adbd786bddfce
tbrunain <tristan@settlemint.com>
2020-06-01 21:30:41 +0200

    feat: Default to bootstrap

--- src/AVA.ts
+++ src/AVA.ts
@@ -7,10 +7,10 @@ const bootstrapChainId = process.env.VUE_APP_SNWBRD_BOOTSTRAP_CHAIN_ID || 'X';
 const bootstrapNodePort = process.env.VUE_APP_SNWBRD_BOOTSTRAP_PORT || '2100';
 const bootstrapNetworkId = process.env.VUE_APP_SNWBRD_BOOTSTRAP_NETWORK_ID || '3';
 
-const nodeHost = process.env.VUE_APP_SNWBRD_NODE_HOST || 'localhost';
-const protocol = process.env.VUE_APP_SNWBRD_NODE_PROTOCOL || 'http';
+const nodeHost = process.env.VUE_APP_SNWBRD_NODE_HOST || 'bootstrap.ava.network';
+const protocol = process.env.VUE_APP_SNWBRD_NODE_PROTOCOL || 'https';
 const chainId = process.env.VUE_APP_SNWBRD_NODE_CHAIN_ID || 'X';
-const nodePort = process.env.VUE_APP_SNWBRD_NODE_PORT || '9650';
+const nodePort = process.env.VUE_APP_SNWBRD_NODE_PORT || '2100';
 const networkId = process.env.VUE_APP_SNWBRD_NODE_NETWORK_ID || '3';
 
 let nodeApi = new slopes.Slopes(nodeHost, parseInt(nodePort), protocol, parseInt(networkId), chainId);
